a60b703c1f1af3959329a73456b77baab6472abb
Alejandro Sergio D'Alessandro <ceo@epa-bienestar.com>
2023-08-31 11:30:11 -0300

    Update LandingPage.tsx
    Traducción mínima al español. MVP

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -6,24 +6,24 @@ import { Link } from 'react-router-dom';
 export function LandingPage(): JSX.Element {
   return (
     <Document>
-      <h1>Welcome!</h1>
+      <h1>Bienvenido!</h1>
       <p>
         This app demonstrates how to use the Medplum SDK to work with{' '}
         <Anchor href="https://www.hl7.org/fhir/task" target="_blank">
-          FHIR Tasks
+          Tareas FHIR
         </Anchor>
         .
       </p>
       <ul>
-        <li>Create a task</li>
-        <li>List tasks</li>
-        <li>Filter tasks</li>
-        <li>Assign a task to a user</li>
-        <li>Complete a task</li>
+        <li>Crear una tarea</li>
+        <li>Lista de tareas</li>
+        <li>Filtrar tareas</li>
+        <li>Asignar tarea a usuario</li>
+        <li>Completar una tarea</li>
       </ul>
       <p>
         <Anchor to="/signin" component={Link}>
-          Sign in
+          Login
         </Anchor>
       </p>
     </Document>
